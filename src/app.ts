2eed8bbb6663645b755dbb204da61eed061b3edd
FelipeVahia <felipevahia@gmail.com>
2022-05-14 22:13:26 -0300

    2) Remove commented code

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,13 @@ app.use(express.json());
 
 app.use('/products', productRoutes);
 
-// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-//   const { name, message, details } = err as any;
-//   console.log(`name: ${name}`);
-//   console.log(`message: ${message}`);
-//   console.log(`details: ${details}`);
-//   res.sendStatus(500);
-//   next();
-// });
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+  console.log(`name: ${name}`);
+  console.log(`message: ${message}`);
+  console.log(`details: ${details}`);
+  res.sendStatus(500);
+  next();
+});
 
 export default app;
