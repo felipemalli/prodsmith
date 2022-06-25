39a559c792d1b0bb4bdcfd33b1024551e9d85a6a
FelipeVahia <felipevahia@gmail.com>
2022-06-25 20:30:41 -0300

    refacotr: use token middleware inside user create

--- src/routers/order.routes.ts
+++ src/routers/order.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import OrderController from '../controllers/order.controller';
+import TokenMiddleware from '../middlewares/token.middleware';
 
 const orderController = new OrderController();
+const tokenValidate = new TokenMiddleware();
 
 const route = Router();
 
 route.get('/', orderController.getAll);
-// route.post('/', orderController.create);
+route.post('/', tokenValidate.authentication, orderController.create);
 
 export default route;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,16 +1,23 @@
-import jwt from 'jsonwebtoken';
 import UserModel from '../models/user.model';
-
-const secretKey = 'process.env.JWT_SECRET';
+import IProduct from '../interfaces/product.interface';
+import TokenMiddleware from '../middlewares/token.middleware';
 
 export default class UserService {
   public model = new UserModel();
+  
+  public tokenMiddleware = new TokenMiddleware();
+
+  public getAll = async (): Promise<IProduct[]> => {
+    const products = await this.model.getAll();
+    return products;
+  };
   
   public create = async (username: string, classe: string, level: number, password: string):
   Promise<string> => {
     const { id } = await this.model.create(username, classe, level, password);
     
-    const token = jwt.sign({ id }, secretKey, { expiresIn: '7d', algorithm: 'HS256' });
+    const token = this.tokenMiddleware.generateToken({ id }, '7d');
+
     return token;
   };
 }
